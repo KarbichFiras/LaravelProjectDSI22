3bcac63a1acf8ee9f0dd04d830ee6b79a893764b
KarbichFiras <firaskarbich@gmail.com>
2021-04-17 02:41:06 +0100

    frontend implementated (npm)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use App\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function welcome(){
-        
-        $products = Product::all();
-        //var_dump($products);
-        return view("welcome" , ["products" => $products]);
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
     }
 }
